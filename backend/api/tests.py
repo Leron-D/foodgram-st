b8540caac58a8af4e3e2bd93834d33b93275a171
LeRoN-D <shangin21072003@gmail.com>
2025-01-03 17:07:00 +0300

    add files

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
 
 
 class FoodgramAPITestCase(TestCase):
@@ -11,10 +12,24 @@ class FoodgramAPITestCase(TestCase):
         response = self.guest_client.get('/api/recipes/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_registration_exists(self):
-        """Проверка доступности страницы регистрации"""
-        response = self.guest_client.get('/localhost/signup')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_user_registration(self):
+        """Проверка регистрации нового пользователя."""
+        user_data = {
+            'username': 'testuser',
+            'password': 'testpassword123',
+            'email': 'testuser@example.com',
+            'first_name': 'Test',
+            'last_name': 'Test'
+        }
+
+        response = self.guest_client.post('/api/auth/register/', data=user_data)
+
+        # Проверяем, что запрос завершился успешно (код 201 Created)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+
+        # Проверяем, что пользователь был создан в базе данных
+        user = get_user_model().objects.get(username='testuser')
+        self.assertEqual(user.email, 'testuser@example.com')
 
     def test_create_recipe_unauthorized(self):
         """Проверка создания рецепта для неавторизованного пользователя (ошибка 401)"""
