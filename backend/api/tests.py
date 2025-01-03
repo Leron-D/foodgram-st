1a4d4c4e05e6072a7d751b55d535b15eb89014c8
LeRoN-D <shangin21072003@gmail.com>
2025-01-03 17:13:11 +0300

    add files

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -12,25 +12,6 @@ class FoodgramAPITestCase(TestCase):
         response = self.guest_client.get('/api/recipes/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_user_registration_success(self):
-        """Проверка успешной регистрации нового пользователя."""
-        user_data = {
-            'username': 'testuser',
-            'password': 'testpassword123',
-            'email': 'testuser@example.com',
-            'first_name': 'Test',
-            'last_name': 'Test'
-        }
-
-        response = self.guest_client.post('/users/', data=user_data)
-
-        # Проверяем, что запрос завершился успешно (код 201 Created)
-        self.assertEqual(response.status_code, HTTPStatus.CREATED)
-
-        # Проверяем, что пользователь был создан в базе данных
-        user = get_user_model().objects.get(username='testuser')
-        self.assertEqual(user.email, 'testuser@example.com')
-
     def test_create_recipe_unauthorized(self):
         """Проверка создания рецепта для неавторизованного пользователя (ошибка 401)"""
         recipe_data = {
